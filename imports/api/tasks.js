11f54e418c84a4f2e32e3cb90e60a14db66f2deb
Sasha Marchuk <sasha.marchuk.email@gmail.com>
2021-07-01 18:20:45 +0300

    add button loadMore with the corresponding functionality. added a deactivation button when loading the full list
    add tmeasday:publish-counts

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -7,10 +7,22 @@ export const Tasks = new Mongo.Collection("tasks");
 if (Meteor.isServer) {
   // This code only runs on the server
   // Only publish tasks that are public or belong to the current user
-  Meteor.publish("tasks", function tasksPublication() {
-    return Tasks.find({
-      $or: [{ private: { $ne: true } }, { owner: this.userId }],
-    });
+  Meteor.publish("tasks", function tasksPublication(limit) {
+    return Tasks.find(
+      {
+        $or: [{ private: { $ne: true } }, { owner: this.userId }],
+      },
+      {
+        limit: limit,
+      },
+      Counts.publish(
+        this,
+        "tasks",
+        Tasks.find({
+          $or: [{ private: { $ne: true } }, { owner: this.userId }],
+        })
+      )
+    );
   });
 }
 
--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,47 +1,71 @@
-import { Meteor } from 'meteor/meteor';
-import { Template } from 'meteor/templating';
-import { ReactiveDict } from 'meteor/reactive-dict';
- 
-import { Tasks } from '../api/tasks.js';
- 
-import './task.js';
-import './body.html';
- 
+import { Meteor } from "meteor/meteor";
+import { Template } from "meteor/templating";
+import { ReactiveDict } from "meteor/reactive-dict";
+
+import { Tasks } from "../api/tasks.js";
+
+import "./task.js";
+import "./body.html";
+
 Template.body.onCreated(function bodyOnCreated() {
-    this.state = new ReactiveDict();
-    Meteor.subscribe('tasks');
+  Session.setDefault("limit", {
+    limit: 2,
   });
- 
-  Template.body.helpers({
-    tasks() {
-        const instance = Template.instance();
-        if (instance.state.get('hideCompleted')) {
-          // If hide completed is checked, filter tasks
-          return Tasks.find({ checked: { $ne: true } }, { sort: { createdAt: -1 } });
-        }
-        // Otherwise, return all of the tasks
-        return Tasks.find({}, { sort: { createdAt: -1 } });
-      },
-      incompleteCount() {
-          return Tasks.find({ checked: { $ne: true } }).count();
-        },
+  Template.instance().autorun(function () {
+    Template.instance().subscribe("tasks", Session.get("limit").limit);
   });
+  this.state = new ReactiveDict();
+  // Meteor.subscribe("tasks");
+});
+
+function updateSession(value) {
+  Session.set("limit", {
+    limit: Session.get("limit").limit + value,
+  });
+}
+Template.body.helpers({
+  tasks() {
+    const instance = Template.instance();
+    if (instance.state.get("hideCompleted")) {
+      // If hide completed is checked, filter tasks
+      return Tasks.find(
+        { checked: { $ne: true } },
+        { sort: { createdAt: -1 } }
+      );
+    }
+    // Otherwise, return all of the tasks
+
+    return Tasks.find({}, { sort: { createdAt: -1 } });
+  },
+  incompleteCount() {
+    return Tasks.find({ checked: { $ne: true } }).count();
+  },
+  isDisabled() {
+    const countAllTasks = Counts.get(`tasks`);
+    return countAllTasks == Tasks.find().count()
+      ? { disabled: "disabled" }
+      : {};
+  },
+});
 Template.body.events({
-'submit .new-task'(event) {
-  // Prevent default browser form submit
-  event.preventDefault();
+  "submit .new-task"(event) {
+    // Prevent default browser form submit
+    event.preventDefault();
 
-  // Get value from form element
-  const target = event.target;
-  const text = target.text.value;
+    // Get value from form element
+    const target = event.target;
+    const text = target.text.value;
 
-  // Insert a task into the collection
-  Meteor.call('tasks.insert', text);
+    // Insert a task into the collection
+    Meteor.call("tasks.insert", text);
 
-  // Clear form
-  target.text.value = '';
-},
-'change .hide-completed input'(event, instance) {
-    instance.state.set('hideCompleted', event.target.checked);
+    // Clear form
+    target.text.value = "";
+  },
+  "change .hide-completed input"(event, instance) {
+    instance.state.set("hideCompleted", event.target.checked);
+  },
+  "click .load-more": function () {
+    updateSession(2);
   },
 });
