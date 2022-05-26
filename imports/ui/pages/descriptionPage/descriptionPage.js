ae63227f75fa67de73458cc386b51c8d53e4e2a0
SashaMarchuk <sasha.marchuk.email@gmail.com>
2022-05-26 11:58:15 +0300

    Add description page for the future feature

---
+++ imports/ui/pages/descriptionPage/descriptionPage.js
@@ -0,0 +1,9 @@
+import { Template } from 'meteor/templating';
+
+import './descriptionPage.html';
+
+Template.descriptionPage.onCreated(function () {});
+Template.descriptionPage.onRendered(function () {});
+Template.descriptionPage.onDestroyed(function () {});
+Template.descriptionPage.helpers({});
+Template.descriptionPage.events({});
