79db54460d509e07988a6591ccb34f5ae3b2f7ac
SashaMarchuk <sasha.marchuk.email@gmail.com>
2022-05-26 12:04:11 +0300

    Update publications for tasks

--- imports/api/tasks/server/publiucation.js
+++ imports/api/tasks/server/publiucation.js
@@ -1,9 +1,11 @@
+import { Meteor } from 'meteor/meteor';
+
 import { Tasks } from "../tasks";
 
-Meteor.publish("tasks", function (limit) {
+Meteor.publish("allTasks", function (limit) {
   const query = {
     $or: [{ private: { $ne: true } }, { owner: this.userId }],
   };
-  const options = { limit };
+  const options = { limit: limit || 10 };
   return Tasks.find(query, options);
 });
