a8df0e6928e16912df949400637847393ea0d368
SashaMarchuk <sasha.marchuk.email@gmail.com>
2022-05-26 12:01:00 +0300

    Refactor admin page

--- imports/ui/pages/adminPage/adminPage.js
+++ imports/ui/pages/adminPage/adminPage.js
@@ -1,24 +1,11 @@
-import { Meteor } from "meteor/meteor";
-import { Template } from "meteor/templating";
-import { ReactiveDict } from "meteor/reactive-dict";
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
 
-import "./adminPage.html";
+import './adminPage.html';
 
-const updateSession = (value) => {
-  Session.set("limit", {
-    limit: Session.get("limit").limit + value,
-  });
-};
+import '../../components/userItem/userItem';
 
-Template.adminPage.onCreated(function () {
-  Session.setDefault("limit", {
-    limit: 4,
-  });
-  Template.instance().autorun(function () {
-    Template.instance().subscribe("userData", Session.get("limit").limit);
-  });
-  this.state = new ReactiveDict();
-});
+Template.adminPage.onCreated(function () {});
 Template.adminPage.onRendered(function () {
   /* ... */
 });
@@ -29,23 +16,5 @@ Template.adminPage.helpers({
   users() {
     return Meteor.users.find({}, { sort: { createdAt: -1 } });
   },
-
-  isDisabled() {
-    const countAllUsers = Counts.get("users");
-    return countAllUsers == Meteor.users.find().count()
-      ? { disabled: "disabled" }
-      : {};
-  },
-  createdAt(createdAt) {
-    return moment(createdAt).format("LLL");
-  },
 });
-Template.adminPage.events({
-  "click .toggle-checked-ban"() {
-    // Set the checked property to the opposite of its current value
-    Meteor.call("users.setCheckedBan", this._id, !this.ban);
-  },
-  "click .load-more": function () {
-    updateSession(5);
-  },
-});
+Template.adminPage.events({});
