1e5011a5e28916c34a14ea6c6985f5686388f277
Sasha Marchuk <sasha.marchuk.email@gmail.com>
2022-06-21 13:36:52 +0300

    Import files

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -1,5 +1,11 @@
 import '../../api/tasks/server/methods';
+import '../../api/votes/server/methods';
+import '../../api/news/server/methods';
 
 import '../../api/tasks/server/publiucation';
+import '../../api/votes/server/publiucation';
+import '../../api/news/server/publiucation';
 
-import '../../api/users/users'
+import '../../api/users/users';
+import '../../api/votes/votes';
+import '../../api/news/news'
